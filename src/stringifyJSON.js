ba39d45ec3b3f6aedee059e7138ede880e3de697
Jackson Christoffersen <jacksontchristo@gmail.com>
2017-09-05 21:40:54 -0700

    stringifyJSON meets spec

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,27 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  if (obj === null) {
+  	return "null";
+  } else if(Array.isArray(obj)) {
+  	var elementStrings = [];
+  	for (var i = 0; i < obj.length; i++) {
+  		if (obj[i] !== undefined && typeof obj[i] !== 'function') {
+  			elementStrings.push((stringifyJSON(obj[i])));
+  		}
+  	}
+  	return "[" + elementStrings.join(",") + "]";
+  } else if(typeof obj === 'object') {
+  	var pairStrings = [];
+  	for (var key in obj) {
+  		if (obj[key] !== undefined && typeof obj[key] !== 'function') {
+  			pairStrings.push(("\"" + key + "\":" + stringifyJSON(obj[key])));
+  		}
+  	}
+  	return "{" + pairStrings.join(",") + "}";
+  } else if (typeof obj === 'string') {
+  	return "\"" + obj + "\"";
+  } else {
+  	return obj.toString();
+  }
 };
